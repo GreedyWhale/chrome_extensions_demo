5641a844c99a6475a7da0154967ab353061e2cad
caisr <383911973@qq.com>
2019-12-16 17:28:05 +0800

    feature：完成GettingStartedExample 扩展的README.md

--- GettingStartedExample/js/options.js
+++ GettingStartedExample/js/options.js
@@ -1,15 +1,15 @@
-let page = document.getElementById('buttonDiv');
+const page = document.getElementById('buttonDiv');
 const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
 function constructOptions(kButtonColors) {
-  for (let item of kButtonColors) {
-    let button = document.createElement('button');
-    button.style.backgroundColor = item;
+  kButtonColors.forEach(color => {
+    const button = document.createElement('button');
+    button.style.backgroundColor = color;
     button.addEventListener('click', function() {
-      chrome.storage.sync.set({color: item}, function() {
-        console.log('color is ' + item);
+      chrome.storage.sync.set({ color }, function() {
+        console.log('color is ' + color);
       })
     });
     page.appendChild(button);
-  }
+  })
 }
 constructOptions(kButtonColors);
